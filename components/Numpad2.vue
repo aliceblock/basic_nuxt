cc27f9dc60980f1c8795a653c9ad99c4550b8c87
Saran Jenjirawat <registbacktown@gmail.com>
2017-06-29 19:45:08 +0700

    Add more component example

---
+++ components/Numpad2.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button type="button" @click="select(1)">1</button>
+    <button type="button" @click="select(2)">2</button>
+    <button type="button" @click="select(3)">3</button>
+    <button type="button" @click="select(4)">4</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    select (number) {
+      this.$emit('update:value', number)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
